fc64916c890ffeb3cbfb5b942d9b3eaea9b13a4a
jkgari0405 <gasoibuki@icloud.com>
2025-08-16 10:40:11 +0900

    soga

--- pra_server.js
+++ pra_server.js
@@ -1,15 +1,23 @@
+// const express = require("express");
+// const app = express();
+// const port = 7777;
+
 const express = require("express");
 const app = express();
 const port = 7777;
 
-app.get("/api/clients", (req, res) => {
-  const clients = [
-    { id: 1, name: "田中" },
-    { id: 2, name: "佐藤" },
-  ];
-  res.json(clients);
-});
-
 app.listen(port, () => {
   console.log(`サーバーが http://localhost:${port} で起動中です！`);
+});
+
+app.get("/", (req, res) => {
+  res.send("APIサーバーが稼働中です");
+});
+
+app.use(express.json());
+
+app.post("/api/clients", (req, res) => {
+  const clientName = req.body.clientName;
+  console.log("受け取った取引先名:", clientName);
+  res.json({ message: "取引先登録が完了しました", clientName });
 });
